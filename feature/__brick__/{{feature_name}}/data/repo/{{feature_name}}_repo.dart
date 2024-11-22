e71610c0e6fdae05247d100926b5d1a0eb73c63b
Kaushal <kaushalk.dev@gmail.com>
2024-11-22 16:52:34 +0530

    added exception handling in repo

--- feature/__brick__/{{feature_name}}/data/repo/{{feature_name}}_repo.dart
+++ feature/__brick__/{{feature_name}}/data/repo/{{feature_name}}_repo.dart
@@ -1,1 +1,21 @@
-class {{feature_name.pascalCase()}}Repo {}
+
+
+class {{feature_name.pascalCase()}}Repo {
+
+
+  Future<void> makeARequest()async {
+
+      try {
+        // make a call
+      }  on TypeError catch (e) {
+      throw BadDataException(e.toString(), StackTrace.current);
+    } on FormatException catch (e) {
+      throw BadDataException(e.toString(), StackTrace.current);
+    } on DioError catch (e) {
+      throw InternetException(e.message, StackTrace.current);
+    } catch (e) {
+      throw UnknownException(e.toString(), StackTrace.current);
+    }
+
+  }
+}
