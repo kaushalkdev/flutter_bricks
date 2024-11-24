c23668bd9b3abe8b2587f400dbe1d3f8a6f92f2b
Kaushal <kaushalk.dev@gmail.com>
2024-11-24 19:57:55 +0530

    wip: added basic states

--- feature/__brick__/{{feature_name}}/ui/{{#use_bloc}}bloc{{/use_bloc}}/{{feature_name}}_state.dart
+++ feature/__brick__/{{feature_name}}/ui/{{#use_bloc}}bloc{{/use_bloc}}/{{feature_name}}_state.dart
@@ -1,3 +1,18 @@
 abstract class {{feature_name.pascalCase()}}State {}
 
 class {{feature_name.pascalCase()}}Initial extends {{feature_name.pascalCase()}}State {}
+
+
+class {{feature_name.pascalCase()}}Loading extends {{feature_name.pascalCase()}}State {}
+
+class {{feature_name.pascalCase()}}Error extends {{feature_name.pascalCase()}}State {
+  final String message;
+
+  {{feature_name.pascalCase()}}Error({required this.message});
+}
+
+class {{feature_name.pascalCase()}}Loaded extends {{feature_name.pascalCase()}}State {
+  final dynamic data;
+
+  {{feature_name.pascalCase()}}Loaded({required this.data});
+}
